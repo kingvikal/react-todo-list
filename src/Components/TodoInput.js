13ae6773b4c2a5ff6992f790302458ed91c00ed3
kingvikal <bikal.kharel2011@gmail.com>
2022-11-11 09:54:28 +0545

    first msg

---
+++ src/Components/TodoInput.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class TodoInput extends Component {
+  render() {
+    return (
+      <div><h1>Hello From ToDoList</h1></div>
+    )
+  }
+}
